325e27f4953509b6de7e0738906f6d5acf0eef0c
Hendrius Félix <hendriusfelix@gmail.com>
2024-11-14 16:41:53 -0300

    dependencias de icones instaladas

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import '@mdi/font/css/materialdesignicons.css'
-import "bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "material-icons/iconfont/filled.css";
+import "material-icons/iconfont/material-icons.css";
+import "material-icons/iconfont/outlined.css";
+import "material-icons/iconfont/round.css";
 
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import VueApexCharts from "vue3-apexcharts"
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import 'vuetify/styles';
+
+import VueApexCharts from "vue3-apexcharts";
 
 const vuetify = createVuetify({
   components,
