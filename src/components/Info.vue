b4a93dafebe36a9b3fd8d196cf5a85231d905059
Hendrius Félix <hendriusfelix@gmail.com>
2024-11-19 16:17:36 -0300

    component para exibição de informações de funcionalidades

---
+++ src/components/Info.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-dialog  max-width="500">
+    <template v-slot:activator="{ props: activatorProps }">
+      <i
+        role="button"
+        v-bind="activatorProps"
+        class="info-button mdi mdi-help-circle-outline text-warning fs-3"
+      ></i>
+    </template>
+
+    <template  v-slot:default="{ isActive }">
+      <v-card class="bg-success" title="Ajuda">
+        <v-card-text>
+          <p>
+            {{ infoDescription }}
+          </p>
+        </v-card-text>
+
+        <v-card-actions>
+					
+          <v-spacer></v-spacer>
+					
+          <v-btn
+            text="Fechar"
+            variant="outlined"
+            color="danger"
+						class="text-white"
+            @click="isActive.value = false"
+          />
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "InfoComponent",
+  components: {},
+  props: {
+    infoDescription: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.info-button {
+	position: absolute;
+	right: 0;
+	top: -10%;
+}
+</style>
