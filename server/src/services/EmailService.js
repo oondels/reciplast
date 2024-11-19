0169a468cd9960f12bad8467185a3c0a5e537470
Hendrius Félix <hendriusfelix@gmail.com>
2024-11-19 16:16:10 -0300

    sessão de ajuda, para relatados de problemas no aplicativo através de email

---
+++ server/src/services/EmailService.js
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import nodeMailer from "nodemailer";
+import checkToken from "../utils/checkToken.js";
+
+const router = Router();
+
+const transporter = nodeMailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+router.post("/send-help-email", checkToken, async (req, res, next) => {
+  try {
+    const { problem, description } = req.body;
+
+    const message = await transporter.sendMail({
+      to: "hendriusfelix.dev@gmail.com",
+      subject: `⚠️Ajuda Reciplast⚠️ - ${problem}`,
+      html: `
+  <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6; max-width: 600px; margin: 0 auto; background-color: #f9f9f9; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+      <div style="text-align: center; margin-bottom: 20px;">
+        <h1 style="color: #d9534f; font-size: 24px; margin: 0;">⚠️ Ajuda Reciplast ⚠️</h1>
+      </div>
+
+      <div style="background-color: #ffffff; padding: 20px; border-radius: 8px; border: 1px solid #e0e0e0;">
+        <h2 style="color: #0056b3; font-size: 20px; margin: 0 0 10px;">Problema Reportado:</h2>
+        <p style="font-size: 16px; color: #555; margin: 10px 0;"><strong>Problema:</strong> ${problem}</p>
+        <p style="font-size: 16px; color: #555; margin: 10px 0;"><strong>Descrição:</strong> ${description}</p>
+      </div>
+
+      <div style="text-align: center; margin-top: 30px; color: #777; font-size: 14px;">
+        <p>Este email foi gerado automaticamente. Por favor, não responda.</p>
+        <p>Em caso de dúvidas, entre em contato pelo suporte: <a href="mailto:support@reciplast.com" style="color: #0056b3; text-decoration: none;">support@reciplast.com</a></p>
+      </div>
+  </div>
+`,
+    });
+
+		return res.status(200).json({ message: "Email enviado com sucesso. Responderei assim que possível." });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
---
+++ src/components/Help.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="fixed-fab">
+    <v-dialog max-width="600">
+      <template v-slot:activator="{ props: activatorProps }">
+        <v-fab v-bind="activatorProps" icon="mdi mdi-chat-question-outline" color="primary" variant="flat"></v-fab>
+      </template>
+
+      <template v-slot:default="{ isActive }">
+        <v-card>
+          <v-card-title
+            class="bg-warning d-flex justify-content-between align-items-center rounded text-white text-center text-bold m-3"
+          >
+            Reportar Problema
+
+            <i class="mdi mdi-close-circle fs-3" role="button" @click="isActive.value = false"></i>
+          </v-card-title>
+
+          <v-card-text>
+            <p class="alert alert-warning rounded">
+              <i class="mdi mdi-information-slab-box fs-5 text-warning"></i>
+              Apenas utilize este recurso se o aplicativo estiver apresentando algum erro.
+            </p>
+
+            <div>
+              <v-combobox
+                :items="[
+                  'Registrar produção',
+                  'Registrar Financeiro',
+                  'Registrar Pedido',
+                  'Gerenciamento',
+                  'Gráficos Errados',
+                  'Informações Erradas',
+                  'Outros',
+                ]"
+                v-model="problema"
+                label="Problema"
+                hint="Selecione o problema que está ocorrendo."
+                persistent-hint
+                clearable
+              />
+
+              <v-text-field
+                v-if="problema === 'Outros'"
+                label="Qual o Problema?"
+                v-model="outroProblema"
+              ></v-text-field>
+
+              <v-textarea
+                :disabled="!problema || (problema === 'Outros' && !outroProblema)"
+                label="Descrição"
+                hint="Descreva o problema com detalhes."
+                v-model="descricao"
+                persistent-hint
+                clearable
+              ></v-textarea>
+
+              <div class="d-flex flex-row justify-content-center align-items-center mt-4">
+                <v-btn
+                  color="success"
+                  :disabled="(!problema && !descricao) || (!outroProblema && !descricao)"
+                  variant="tonal"
+                  prepend-icon="mdi mdi-send-circle"
+                  @click="sendEmail"
+                  :loading="loadingEmail"
+                >
+                  Enviar
+
+                  <template v-slot:loader>
+                    <v-progress-circular indeterminate color="white"></v-progress-circular>
+                  </template>
+                </v-btn>
+
+                <v-btn
+                  class="ml-1"
+                  color="warning"
+                  :disabled="(!problema && !descricao) || (!outroProblema && !descricao)"
+                  variant="tonal"
+                  prepend-icon="mdi mdi-close"
+                  @click="clear"
+                >
+                  Limpar
+                </v-btn>
+              </div>
+            </div>
+          </v-card-text>
+        </v-card>
+      </template>
+    </v-dialog>
+
+    <alert ref="alert" />
+  </div>
+</template>
+
+<script>
+import Alert from "@/components/Alert.vue";
+import axios from "axios";
+import ip from "../ip";
+
+export default {
+  name: "HelpComponent",
+  components: { Alert },
+
+  data() {
+    return {
+      loadingEmail: false,
+      problema: "",
+      outroProblema: "",
+      descricao: "",
+    };
+  },
+
+  mounted() {},
+  methods: {
+    clear() {
+      this.problema = "";
+      this.outroProblema = "";
+      this.descricao = "";
+    },
+
+    sendEmail() {
+      if (this.problema === "Outros") {
+        this.problema = this.outroProblema;
+      }
+
+      this.loadingEmail = !this.loadingEmail;
+      axios
+        .post(`${ip}/email/send-help-email`, {
+          problem: this.problema,
+          description: this.descricao,
+        })
+        .then((response) => {
+          this.loadingEmail = !this.loadingEmail;
+          this.$refs.alert.mostrarAlerta(
+            "success",
+            "check_circle",
+            "Sucesso",
+            response.data.message
+          );
+					this.clearEmail();
+        })
+        .catch((error) => {
+          this.loadingEmail = !this.loadingEmail;
+          console.error("Erro ao enviar email: ", error);
+          this.$refs.alert.mostrarAlerta("warning", "warning", "Erro", error.response.data.message);
+        });
+    },
+
+		clearEmail() {
+			this.problema = "";
+			this.outroProblema = "";
+			this.descricao = "";
+		},
+  },
+};
+</script>
+
+<style scoped>
+.fixed-fab {
+  position: fixed !important;
+  bottom: 50px !important;
+  right: 100px !important;
+  z-index: 1000 !important;
+}
+</style>
