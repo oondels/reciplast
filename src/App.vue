6573283773e9ad57419f554eac2f2088f8d10b18
Hendrius Félix <hendriusfelix@gmail.com>
2024-11-19 16:16:36 -0300

    proteção de acesso a dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,45 @@
 <template>
-  <SideNav :showSideNav="showSideNav" />
+  <SideNav v-if="pageName() !== 'Autenticação'" :showSideNav="showSideNav" />
   <main class="main-content position-relative max-height-vh-100 h-100 overflow-x-hidden">
-    <NavBar @toggle-sidebar="toggleSideMenu" />
+    <NavBar v-if="pageName() !== 'Autenticação'" @toggle-sidebar="toggleSideMenu" />
     <router-view />
+
+    <Help v-if="decodeJwt()" />
   </main>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
 import SideNav from "@/components/SideNav.vue";
+import VueJwtDecode from "vue-jwt-decode";
+import Help from "./components/Help.vue";
 
 export default {
   name: "App",
-  components: { NavBar, SideNav },
+  components: { NavBar, SideNav, Help },
   data() {
-    return { 
-			showSideNav: false,
-		};
+    return {
+      showSideNav: false,
+    };
   },
 
+  mounted() {},
+
   methods: {
+    decodeJwt() {
+      let token = sessionStorage.getItem("token");
+      if (token) {
+        return VueJwtDecode.decode(token);
+      }
+    },
+
     toggleSideMenu() {
       this.showSideNav = !this.showSideNav;
     },
 
+    pageName() {
+      return this.$route.name;
+    },
   },
 };
 </script>
@@ -36,7 +52,7 @@ export default {
   color: #2c3e50;
   background-color: #f0f2f5;
   display: flex;
-	overflow: hidden !important;
+  overflow: hidden !important;
 }
 
 main {
